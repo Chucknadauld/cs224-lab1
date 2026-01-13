8e5a43eb33f3129d8eaa3908c4f0a134ff58fd55
Chuck Nadauld <charlienadauld@gmail.com>
2026-01-12 21:09:13 -0700

    finalize

--- src.c
+++ src.c
@@ -3,12 +3,12 @@
 int main(int argc, char *argv[])
 {
 
-	int first = 0;
-	int second = 0;
+	int first = 270;
+	int second = 289;
 
 	int result = (first & second) - (first >> 4) - ((second & 0x20) >> 1);
 
-	printf("My name is Cosmo Cougar\n");
+	printf("My name is Chuck\n");
 	printf("The first number is %d\nThe second number is %d\n", first, second);
 	printf("The result is %d\n", result);
 
